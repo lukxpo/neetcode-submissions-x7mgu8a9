4516036fff3f02123e83e8225b711a245053bcd7
lukxpo <lucas.r.esposito@gmail.com>
2026-04-02 15:01:58 -0300

    Delete duplicate submission

--- Data Structures & Algorithms/duplicate-integer/submission-1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        checked_nums = set()
-        for num in nums:
-            if num in checked_nums:
-                return True
-            checked_nums.add(num)
-        return False
